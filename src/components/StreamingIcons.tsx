308caf8f1ab3798f053c6650fffb52f2ede69cb2
Rene Plowden <reneplowden@Renes-MacBook-Pro.local>
2025-01-22 03:50:43 -0500

    link updates

--- src/components/StreamingIcons.tsx
+++ src/components/StreamingIcons.tsx
@@ -5,9 +5,9 @@ import "./StreamingIcons.css";
 
 function StreamingIcons() {
   const icons = [
-    { icon: faSpotify, title: 'Spotify', link: 'https://open.spotify.com/artist/3FKooCA03v1OT8flP2jHR2?si=O8hPFquISrKf90Ek96K2ag' },
-    { icon: faItunesNote, title: 'Apple Music', link: 'https://music.apple.com/us/artist/rene/858233' },
-    { icon: faBandcamp, title: 'Bandcamp', link: 'https://rene.bandcamp.com/' },
+    { icon: faSpotify, title: 'Spotify', link: 'https://open.spotify.com/track/6sZFziOX3czLINf7FrQMCt?si=9116bdbd2b80402e' },
+    { icon: faItunesNote, title: 'Apple Music', link: 'https://music.apple.com/us/album/unraveling-single/1788722071' },
+    { icon: faBandcamp, title: 'Bandcamp', link: 'https://rene.bandcamp.com/track/unraveling' },
       // { icon: faYoutube, title: 'YouTube', link: 'https://www.youtube.com/@renebeatsandcoffee' },
     ];
   return (
@@ -24,7 +24,7 @@ function StreamingIcons() {
           </a>
         </div>
         ))}
-        <div className="icon-item">
+        {/* <div className="icon-item">
           <a href="https://tidal.com/browse/artist/41378693" className="streaming-link" target="_blank" rel="noopener noreferrer" style={styles.icon}>
             <div className="icon">
               <svg xmlns="http://www.w3.org/2000/svg" fill="#479cff" width="50px" height="45px" viewBox="0 0 32 32">
@@ -34,7 +34,7 @@ function StreamingIcons() {
             <div className="streaming-icon-title">Tidal</div>
             <div className="listen">Listen</div>
           </a>
-        </div>
+        </div> */}
         {/* <div className="icon">
           <svg xmlns="http://www.w3.org/2000/svg" fill="#479cff" width="50px" height="45px" viewBox="0 0 32 32">
             <path d="M16.016 5.323l-5.339 5.339-5.339-5.339-5.339 5.339 5.339 5.339 5.339-5.339 5.339 5.339-5.339 5.339 5.339 5.339 5.339-5.339-5.339-5.339 5.339-5.339zM21.391 10.661l5.302-5.307 5.307 5.307-5.307 5.307z"/>
